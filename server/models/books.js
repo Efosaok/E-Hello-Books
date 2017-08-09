639916a5c15168e9da78ae0220a32f43a7f57613
efosa <efosaokpugie@gmail.com>
2017-08-09 09:29:22 +0100

    databse setup using sequelize folder

---
+++ server/models/books.js
@@ -0,0 +1,37 @@
+'use strict';
+export default (sequelize, DataTypes)=> {
+  const Books = sequelize.define('Books', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    isclicense: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    contents: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    classMethods: {
+      associate: (models)=> {
+        // associations can be defined here
+      }
+    }
+  });
+  return Books;
+};
+
---
+++ server/models/users.js
@@ -0,0 +1,41 @@
+'use strict';
+export default (sequelize, DataTypes)=> {
+  const Users = sequelize.define('Users', {
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    othername: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    borrowedbooks: {
+      type: DataTypes.STRING
+    },
+    membershipLevel: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue : 'Silver'
+    }
+  }, {
+    classMethods: {
+      associate: (models)=> {
+        // associations can be defined here
+      }
+    }
+  });
+  return Users;
+};
